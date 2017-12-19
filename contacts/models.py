9086864b23d0252726f8e51b0d068d4d9ef28de0
Anu Apiti <anuapt@gnail.com>
2017-12-19 11:17:16 -0800

    Feat: set up contacts app model with fields validation with migrations

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    """
+     Model for contact instance.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular contact across whole address book")
+    first_name = models.CharField(max_length=100, help_text="Please fill your first name")
+    last_name = models.CharField(max_length=100, help_text="Please fill your last name")
+    email = models.EmailField(max_length=100, help_text="Please fill your email address")
+
+
+
+    def get_absolute_url(self):
+
+        return reverse('contact-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+
+        return '%s, %s' % (self.last_name, self.first_name, self.email)
