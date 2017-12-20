9bc17e5750c06b95c44fb377dfdd3dbc20e1b8c9
Anu Apiti <anuapt@gnail.com>
2017-12-20 05:02:44 -0800

    Feat: contact list page returns added listing through admin

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^list/', views.list, name='list'),
-    url(r'^add/', views.add, name='add')
+    url(r'^list/$', views.ContactListView.as_view(), name='contacts'),
+    url(r'^list/(?P<pk>\d+)$', views.ContactDetailView.as_view(), name='contact-detail'),
+    url(r'^add/$', views.add, name='add')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Contact
 
 # Create your views here.
 
+class ContactListView(generic.ListView):
+    model = Contact
+
+class ContactDetailView(generic.DetailView):
+    model = Contact
+
 def index(request):
     return render(request,'index.html')
-
-
-def list(request):
-    return render(request,'list.html')
 
 def add(request):
     return render(request,'add.html')
