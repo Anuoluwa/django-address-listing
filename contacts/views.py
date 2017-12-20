93fd51e3dd7592e7fd8bc7f670d473360b9c31ba
Anu Apiti <anuapt@gnail.com>
2017-12-20 12:08:34 -0800

    Feat: create CRUD operations for contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Contact
 
 # Create your views here.
@@ -14,5 +16,22 @@ class ContactDetailView(generic.DetailView):
 def index(request):
     return render(request,'index.html')
 
-def add(request):
-    return render(request,'add.html')
+
+class ContactCreate(CreateView):
+    model = Contact
+    fields = '__all__'
+    success_url = reverse_lazy('contacts')
+
+class ContactUpdate(UpdateView):
+    model = Contact
+    fields = '__all__'
+    success_url = reverse_lazy('contacts')
+
+
+class ContactDelete(DeleteView):
+    model = Contact
+    """
+        reverse_lazy() is a lazily executed version of reverse(), used because I am providing
+        a URL to a class-based view attribute.
+    """
+    success_url = reverse_lazy('contacts')
